e6f903e124874d364bb67fa4effd8bc1e2490f98
geekkon <neetro@mail.ru>
2020-05-09 21:34:38 +0300

    FeedImageCell image loading logic & tests

---
+++ MyEssentialFeedIOS/FeedImageCell.swift
@@ -0,0 +1,29 @@
+//
+//  FeedImageCell.swift
+//  MyEssentialFeedIOS
+//
+//  Created by Dim on 08.05.2020.
+//  Copyright © 2020 Dmitriy Baklanov. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    public let feedImageContainer = UIView()
+    public let feedImageView = UIImageView()
+
+    public private (set) lazy var feedImageRetryButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    var onRetry: (() -> Void)?
+
+    @objc private func retryButtonTapped() {
+        onRetry?()
+    }
+}
